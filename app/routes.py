c150db73aeecbaddb9817864437f72c51033880f
Laurel <dawsonlaurel@gmail.com>
2014-04-24 15:20:49 -0500

    Adding list to routes

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
 from flask import render_template
 from app import app
 from ClarificationResponder import clarify
+from parseTextFile import getListFromFile
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+   return render_template('index.html', dict=getListFromFile('../data/dictionary.txt'))
 
 @app.route('/clarify')
 def clarifyRender():
