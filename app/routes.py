8b8c080123cc3b80b220ec05cae1a977337aa108
Ryan Flaherty <Ryan.Flaherty2@gmail.com>
2014-04-24 18:06:36 -0500

    ifs?

--- app/routes.py
+++ app/routes.py
@@ -3,11 +3,11 @@ from app import app
 from ClarificationResponder import clarify
 from parseTextFile import getSetFromFile
 
-tableSize = 5
+setSize = 25
 
 @app.route('/')
 def home():
-   return render_template('index.html', bingoSet=getSetFromFile('data/dictionary.txt', tableSize*tableSize), tableSize=tableSize)
+   return render_template('index.html', bingoSet=getSetFromFile('data/dictionary.txt', setSize))
 
 @app.route('/clarify')
 def clarifyRender():
