8bc71952b7468653f247e72fff9f240914b50fa5
Darles Barkley <spook6@gmail.com>
2014-04-24 13:42:43 -0500

    Updated ClarificationResponder to have multiple responses

--- ClarificationResponder.py
+++ ClarificationResponder.py
@@ -1,2 +1,19 @@
+from random import randint
 clarification = raw_input("Please Enter Your Clarification: ")
-print 'Synergy'
+x=randint(0,2)
+
+def syn():
+        print "Synergy \n"
+
+def acc():
+        print "Accept as Delivered \n"
+
+def rai():
+        print "Raise a Defect \n"
+
+options = {0 : syn,
+		1 : acc,
+		2 : rai,
+}
+
+options[x]()
