0903a1ed1d0c54cd2dbe396d7ea5c98a978717f5
NuRelic <bnowlin1@trinity.edu>
2014-04-25 12:26:53 -0500

    Changing I/O of dictionary in parseTextFile

--- app/parseTextFile.py
+++ app/parseTextFile.py
@@ -1,16 +1,26 @@
 import os.path
 from random import randint
 
-def getListFromFile(file):
+def getListFromFile(file, size):
    if os.path.isfile(file):
-      #return a list of lines
-      return [line.strip() for line in open(file)]
+		#return a list of lines
+		list = [line.strip() for line in open(file)]
+		if (len(list) != len(set(list)):
+			getListFromFile(file, size)
+		list = insertBlank(list, size)
+		return list
    else:
       #return an empty list
       return list()
+	  
+def insertBlank(list, size)
+	index = int(size/2)
+	list[index] = ''
+	return list
+	
 
 def getSetFromFile(file, size):
-	list = getListFromFile(file)
+	list = getListFromFile(file, size)	
 	randSet = set()
 	if len(list) != 0:
 		while len(randSet) < size:
