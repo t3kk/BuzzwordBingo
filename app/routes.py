0e2ba9c1859e3ff3698b6ab47f1a5ec4f953720f
NuRelic <bnowlin1@trinity.edu>
2014-04-25 09:58:45 -0500

    Trying to fix import things

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ setSize = 25
 
 @app.route('/')
 def home():
-   return render_template('index.html', bingoSet=getSetFromFile('data/dictionary.txt', setSize))
+   return render_template('index.html', bingoSet=parseTextFile.getSetFromFile('data/dictionary.txt', setSize))
 
 @app.route('/clarify')
 def clarify():
