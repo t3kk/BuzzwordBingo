45d8af49892fca41f799a6a755061f4392fc5fba
Ryan Flaherty <Ryan.Flaherty2@gmail.com>
2014-04-26 13:27:52 -0500

    Testing checking coordinates.

--- app/static/js/bingo.js
+++ app/static/js/bingo.js
@@ -2,6 +2,12 @@
 
 function bingoCellClicked(clickedId)
 {
+	toggleColor(clickedId);
+	checkForWin(clickedId);
+}
+
+function toggleColor(clickedId)
+{
 	if (document.getElementById(clickedId).style.backgroundColor!="red")
 	{
 		document.getElementById(clickedId).style.backgroundColor="red";
@@ -10,4 +16,21 @@ function bingoCellClicked(clickedId)
 	{
 		document.getElementById(clickedId).style.backgroundColor="white";
 	}
+}
+
+function checkForWin(clickedId)
+{
+	extractCoordinates(clickedId);
+}
+
+function extractCoordinates(clickedId)
+{
+	//Used txt2re.com to make this function cause its soo redicilously easy.  Please suggest improvements if you notice them.
+	var re1='.*?';	// Non-greedy match on filler
+    var re2='(\\d+)';	// Integer Number 1
+    var re3='.*?';	// Non-greedy match on filler
+    var re4='(\\d+)';	// Integer Number 2
+    var p = new RegExp(re1+re2+re3+re4,["i"]);
+    var m = p.exec(clickedId);
+    alert(m[0]+''+m[1]);
 }
